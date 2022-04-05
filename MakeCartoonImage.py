fd203d9da5d9c989c35c2ba1cd311a954ad3f9c5
Scott Stewart <sstewart6147@gmail.com>
2022-04-04 20:20:14 -0500

    Update MakeCartoonImage.py

--- MakeCartoonImage.py
+++ MakeCartoonImage.py
@@ -9,200 +9,61 @@ License: MIT
 
 import cv2 
 import numpy as np 
+import signal
+import sys
 
-def make_cartoon_image(ImagePath, #input image path
-                  cartoonPath, #output image path
+
+def sigint_handler(signal, frame):
+    print ('KeyboardInterrupt. Program Exiting')
+    vral = False
+    globals()["v_all"] = False
+    #sys.exit(0)
+
+def live_cartoon_image(originalImage, #input image path
+                  BackgroundImage,
+                  NewBackground,
                   AdaptiveThreshold1=25, #The lower this is the more lines appear
                   AdaptiveThreshold2=9, #The higher this is the less lines appear. must be less than AdaptiveThreshold1
-                  ThickerLinesIterations = 3, #The higher this is the thicker lines will be
-                  RemoveDotsIterations = 3,  #The higher this is the thinner lines will be. Removes some noisy dots as well
-                  BlurIterations=5, #The higher this is the more cartoonish the image will look, but too high and it will all be one color
-                  ValueIncrease = 1.5,#The higher this is the brigher the colors will be
-                  shadow = 1.3, #The higher this is the more colorless the shadows will be
-                  saturationIncrease = 2, #The higher this is the more cartoonish the colors will be
-                  fade = 0.9, #The higher this is the darker shadows will be
-                  buckets = 15): #The higher this is the more colors in the image.
-    
-    
-    
-    originalmage = cv2.imread(ImagePath)
-    if originalmage is None:
-        raise FileNotFoundError("Image Couldn't be found")
-    
-        
-    #Create and blur greyscale image. This is used by the adaptive thresholding
-    grayScaleImage = cv2.cvtColor(originalmage, cv2.COLOR_BGR2GRAY)
-    smoothGrayScale = cv2.medianBlur(grayScaleImage, 5)
-    #Create an adaptive threshold mask. This creates dark lines where there are objects
-    getEdge = cv2.adaptiveThreshold(smoothGrayScale, 255, cv2.ADAPTIVE_THRESH_MEAN_C,
-                                    cv2.THRESH_BINARY, AdaptiveThreshold1, AdaptiveThreshold2)
-    
-    
-    #Create the color image in order to blur it
-    colorImage = originalmage
-    for i in range(BlurIterations):
-        #Blur the image. This will improve the results of the bucketing later
-        colorImage = cv2.blur(colorImage, (15, 15))
-    cartoonImage = colorImage
-    #cartoonImage = cv2.bitwise_and(cartoonImage, cartoonImage, mask=getEdge)
-    
-    #Create a canny edge detection mask from the original image
-    canny = cv2.Canny(originalmage, 150, 50,(5,5))
-    canny = cv2.bitwise_not(canny)
-    
-    #Erode the inverse of the mask, this will make the lines more noticable 
-    canny = cv2.erode(canny, (3,3), iterations=ThickerLinesIterations)
-    
-    #Combine the adaptive threshold and the canny masks
-    canny = cv2.bitwise_and(canny, canny, mask=getEdge)
-    
-    #Dilate the results. This will get rid of a few extra dots.
-    canny = cv2.dilate(canny, (3,3), iterations=RemoveDotsIterations)
-    
-    
-    #Convert the image to hsv and remove the component parts
-    hsv = cv2.cvtColor(cartoonImage, cv2.COLOR_BGR2HSV)
-    h, s, v = hsv[:, :, 0], hsv[:, :, 1], hsv[:, :, 2]
-
-    #Bucket the h values and assign each pixel in the bucket
-    # the average value of each bucket. This creates
-    # an effect where the the image has strong lines
-    h_new = h.copy()
-    amax = np.amax(h)
-    for b in range(buckets):
-        
-       minimum = int(round(amax/buckets*b))
-       maximum = int(round(amax/buckets*(b+1)))
-       try:
-           set_value = int(round(np.average(h, weights=((h >= minimum) & (h < maximum)))))
-       except:
-           #There is nothing in this bin.
-           set_value = 0
-       
-       h_new = np.where((h >= minimum) & (h < maximum), set_value, h)
-    
-    #Modify the saturation of each pixel by a factor of saturationIncrease. if that 
-    # is greater than the max value it becomes the max value. Otherwise it gets fadded by a
-    #factor of fade
-    #This brings out colors in the image making it look more cartoonish, except low saturation
-    #areas which are even more colorless
-    s = np.where(s*saturationIncrease >= 255, 255, s/fade)
-    
-    #Modify the value of each pixel by a factor of value increase. if that 
-    # is greater than the max value it becomes the max value. Otherwise it gets fadded by a
-    #factor of shadow
-    #This brings out colors in the image making it look more cartoonish, except shadows
-    #which are more dramatic. Makes more dramatic lines
-    v = np.where(v*ValueIncrease >= 255, 255, v/shadow) #Switch these around for a cool effect
-    
-    #Recreate the hsv image
-    hsv[:,:,0] = h_new
-    hsv[:,:,1] = s
-    hsv[:,:,2] = v
-    
-    #Convert back to bgr
-    cartoonImage = cv2.cvtColor(hsv, cv2.COLOR_HSV2BGR)
-    #Add the masks made earlier to the image
-    cartoonImage = cv2.bitwise_and(cartoonImage, cartoonImage, mask=canny)
-    #Save the image
-    cv2.imwrite(cartoonPath, cartoonImage)
-
-def make_cartoon_image2(ImagePath, #input image path
-                  cartoonPath, #output image path
-                  AdaptiveThreshold1=25, #The lower this is the more lines appear
-                  AdaptiveThreshold2=9, #The higher this is the less lines appear. must be less than AdaptiveThreshold1
-                  ThickerLinesIterations = 3, #The higher this is the thicker lines will be
-                  RemoveDotsIterations = 15,  #The higher this is the thinner lines will be. Removes some noisy dots as well
+                  ThickerLinesIterations = 5, #The higher this is the thicker lines will be
+                  RemoveDotsIterations = 10,  #The higher this is the thinner lines will be. Removes some noisy dots as well
                   BlurIterations=2, #The higher this is the more cartoonish the image will look, but too high and it will all be one color
                   ValueIncrease = 1.5,#The higher this is the brigher the colors will be
                   shadow = 1.1, #The higher this is the more colorless the shadows will be
                   saturationIncrease = 2, #The higher this is the more cartoonish the colors will be
                   fade = 0.9, #The higher this is the darker shadows will be
-                  buckets = 7): #The higher this is the more colors in the image.
-    
-    
-    
-    originalmage = cv2.imread(ImagePath)
-    if originalmage is None:
-        raise FileNotFoundError("Image Couldn't be found")
-    
-        
-    #Create and blur greyscale image. This is used by the adaptive thresholding
-    grayScaleImage = cv2.cvtColor(originalmage, cv2.COLOR_BGR2GRAY)
-    smoothGrayScale = cv2.medianBlur(grayScaleImage, 5)
-    #Create an adaptive threshold mask. This creates dark lines where there are objects
-    getEdge = cv2.adaptiveThreshold(smoothGrayScale, 255, cv2.ADAPTIVE_THRESH_MEAN_C,
-                                    cv2.THRESH_BINARY, AdaptiveThreshold1, AdaptiveThreshold2)
-    
-    
-    #Create the color image in order to blur it
-    colorImage = originalmage
-    for i in range(BlurIterations):
-        #Blur the image. This will improve the results of the bucketing later
-        colorImage = cv2.blur(colorImage, (15, 15))
-    cartoonImage = colorImage
-    #cartoonImage = cv2.bitwise_and(cartoonImage, cartoonImage, mask=getEdge)
-    
-    #Create a canny edge detection mask from the original image
-    canny = cv2.Canny(originalmage, 150, 50,(5,5))
-    canny = cv2.bitwise_not(canny)
-    
-    #Erode the inverse of the mask, this will make the lines more noticable 
-    canny = cv2.erode(canny, (3,3), iterations=ThickerLinesIterations)
-    
-    #Combine the adaptive threshold and the canny masks
-    canny = cv2.bitwise_and(canny, canny, mask=getEdge)
-    
-    canny = getEdge
-    
-    #Dilate the results. This will get rid of a few extra dots.
-    canny = cv2.dilate(canny, (3,3), iterations=RemoveDotsIterations)
-    
+                  buckets = 7): #The higher this is the more colors in the image. 
     
     #Convert the image to hsv and remove the component parts
-    hsv = cv2.cvtColor(cartoonImage, cv2.COLOR_BGR2HSV)
-    h, s, v = hsv[:, :, 0], hsv[:, :, 1], hsv[:, :, 2]
-    h_new = h.copy()
-    amax = np.amax(h)
-    for b in range(buckets):
-        
-       minimum = int(round(amax/buckets*b))
-       maximum = int(round(amax/buckets*(b+1)))
-       try:
-           set_value = int(round(np.average(h, weights=((h >= minimum) & (h < maximum)))))
-       except:
-           #There is nothing in this bin.
-           set_value = 0
-       
-       h_new = np.where((h >= minimum) & (h < maximum), set_value, h)
-    #Bucket the h values and assign each pixel in the bucket
-    # the average value of each bucket. This creates
-    # an effect where the the image has strong lines
     
     
-    #Modify the saturation of each pixel by a factor of saturationIncrease. if that 
-    # is greater than the max value it becomes the max value. Otherwise it gets fadded by a
-    #factor of fade
-    #This brings out colors in the image making it look more cartoonish, except low saturation
-    #areas which are even more colorless
-    s = np.where(s*saturationIncrease >= 255, 255, s/fade)
     
-    #Modify the value of each pixel by a factor of value increase. if that 
-    # is greater than the max value it becomes the max value. Otherwise it gets fadded by a
-    #factor of shadow
-    #This brings out colors in the image making it look more cartoonish, except shadows
-    #which are more dramatic. Makes more dramatic lines
-    v = np.where(v*ValueIncrease >= 255, 255, v/shadow) #Switch these around for a cool effect
+    backSub = cv2.createBackgroundSubtractorKNN()
+    for i in range(10):
+        fgMask = backSub.apply(BackgroundImage)
+    fgMask = backSub.apply(originalImage)
     
-    #Recreate the hsv image
-    hsv[:,:,0] = h_new
-    hsv[:,:,1] = s
-    hsv[:,:,2] = v
+    #print(fgMask)
+    fgMask = cv2.resize(fgMask, (originalImage.shape[1],originalImage.shape[0]))
+    fgMask = np.where(fgMask > 250, 1, 0).astype(np.uint8)
+    for i in range(BlurIterations):
+    #Blur the image. This will improve the results of the bucketing later
+        fgMask = cv2.blur(fgMask, (15, 15))
     
+    fgMask = cv2.erode(fgMask, (5,5), iterations=ThickerLinesIterations)
+    fgMask = cv2.dilate(fgMask, (3,3), iterations=RemoveDotsIterations)
+    
+    
+    #maskedImage = cv2.bitwise_and(originalImage, originalImage, mask=fgMask)
+    #background = cv2.bitwise_and(NewBackground, NewBackground, mask=cv2.bitwise_not(fgMask))
+    #maskedImage += background
+
+    hsv = cv2.cvtColor(originalImage, cv2.COLOR_BGR2HSV)
+
     h, s, v = hsv[:, :, 0], hsv[:, :, 1], hsv[:, :, 2]
     h_new = h.copy()
     s_new = s.copy()
     v_new = v.copy()
+    grayScaleImage = cv2.cvtColor(originalImage, cv2.COLOR_BGR2GRAY)
     amax = np.amax(grayScaleImage)
     
     for b in range(buckets):
@@ -210,24 +71,77 @@ def make_cartoon_image2(ImagePath, #input image path
        minimum = int(round(amax/buckets*b))
        maximum = int(round(amax/buckets*(b+1)))
        try:
-           set_value = int(round(np.average(h, weights=((grayScaleImage >= minimum) & (grayScaleImage < maximum)))))
-           set_values = int(round(np.average(s, weights=((grayScaleImage >= minimum) & (grayScaleImage < maximum)))))
-           set_valuev = int(round(np.average(v, weights=((grayScaleImage >= minimum) & (grayScaleImage < maximum)))))
+           set_value = int(round(np.average(h, weights=((grayScaleImage >= minimum) & (grayScaleImage < maximum)  & (fgMask == 1)))))
+           
+           set_values = int(round(np.average(s, weights=((grayScaleImage >= minimum) & (grayScaleImage < maximum) & (fgMask == 1)))))
+           set_valuev = int(round(np.average(v, weights=((grayScaleImage >= minimum) & (grayScaleImage < maximum) & (fgMask == 1)))))
+           
        except:
            #There is nothing in this bin.
            set_value = 0
+           set_values = 0
+           set_valuev = 0
        
-       h_new = np.where((grayScaleImage >= minimum) & (grayScaleImage < maximum), set_value, h)
-       s_new = np.where((grayScaleImage >= minimum) & (grayScaleImage < maximum), set_values, s)
-       v_new = np.where((grayScaleImage >= minimum) & (grayScaleImage < maximum), set_valuev, v)
+       h_new = np.where((grayScaleImage >= minimum) & (grayScaleImage < maximum) & (fgMask == 1), h_new, h_new)
+       s_new = np.where((grayScaleImage >= minimum) & (grayScaleImage < maximum) & (fgMask == 1), set_values, s_new)
+       v_new = np.where((grayScaleImage >= minimum) & (grayScaleImage < maximum) & (fgMask == 1), set_valuev, v_new)
+       
+    
     hsv[:,:,0] = h_new
     hsv[:,:,1] = s_new
     hsv[:,:,2] = v_new
     #Convert back to bgr
-    cartoonImage = cv2.cvtColor(hsv, cv2.COLOR_HSV2BGR)
-    #Add the masks made earlier to the image
-    cartoonImage = cv2.bitwise_and(cartoonImage, cartoonImage, mask=canny)
-    #Save the image
-    cv2.imwrite(cartoonPath, cartoonImage)
     
-make_cartoon_image2("/home/scott/Downloads/Scotts.jpg","/home/scott/Downloads/Scott-CE3.jpg")
+    
+    
+    cartoonImage = cv2.cvtColor(hsv, cv2.COLOR_HSV2BGR)
+    
+    canny = cv2.Canny(cartoonImage, 150, 50,(5,5))
+    canny = cv2.bitwise_not(canny)
+    cartoonImage = cv2.bitwise_and(cartoonImage, cartoonImage, mask=canny)
+    
+    
+    maskedImage = cv2.bitwise_and(cartoonImage, cartoonImage, mask=fgMask)
+    background = cv2.bitwise_and(NewBackground, NewBackground, mask=cv2.bitwise_not(fgMask))
+    maskedImage += background
+    maskedImage = cv2.bitwise_and(maskedImage, maskedImage, mask=canny)
+    #Add the masks made earlier to the image
+    #cartoonImage = cv2.bitwise_and(cartoonImage, cartoonImage, mask=canny)
+    #Save the image
+    #cv2.imwrite(cartoonPath, cartoonImage)
+    return maskedImage
+def getWebcam():
+    
+    new_background = cv2.imread("/home/scottstewart/Pictures/Green.png")
+    cv2.namedWindow("preview")
+    vc = cv2.VideoCapture(-1)
+    FirstFrame = None
+    fgMask = None
+    if vc.isOpened(): # try to get the first frame
+        rval, frame = vc.read()
+        if(FirstFrame == None):
+            FirstFrame = frame
+            new_background = cv2.resize(new_background, (FirstFrame.shape[1],FirstFrame.shape[0]))
+            
+    else:
+        rval = False
+    
+    while (rval and globals()["v_all"]):
+       
+        frame = np.flip(live_cartoon_image(frame,FirstFrame, new_background),1)
+        cv2.imshow("preview", frame)
+        rval, frame = vc.read()
+        key = cv2.waitKey(2)
+        if(key == ord('`')):
+            FirstFrame = frame
+        if key == 27: # exit on ESC
+            break
+    
+    vc.release()
+    cv2.destroyWindow("preview")
+ 
+globals()["v_all"] = True
+signal.signal(signal.SIGINT, sigint_handler)
+getWebcam()
+    
+#make_cartoon_image2("/home/scott/Downloads/Scotts.jpg","/home/scott/Downloads/Scott-CE3.jpg")
